03651d228feae6635374c3b03f7a525e0852e688
Andrei Voronin <nihlete@gmail.com>
2025-12-07 19:57:21 +0400

    added support for ints, floats, and strings
    also added a test cases

--- grammar.js
+++ grammar.js
@@ -10,8 +10,83 @@
 module.exports = grammar({
   name: "quirrel",
 
+  extras: ($) => [
+    /\s/, // whitespace
+    $.comment,
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._statement),
+
+    _statement: $ => choice(
+      $.local_statement,
+      $.let_statement
+    ),
+
+    local_statement: $ => seq(
+      'local',
+      $.identifier,
+      '=',
+      $.expression
+    ),
+
+    let_statement: $ => seq(
+      'let',
+      $.identifier,
+      '=',
+      $.expression
+    ),
+
+    expression: $ => choice(
+      $.identifier,
+      $.number,
+      $.string,
+    ),
+
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+
+    number: $ => choice(
+      $.integer,
+      $.float
+    ),
+
+    integer: $ => token(choice(
+      /-?[1-9][0-9_]*/,
+      /0[xX][0-9A-Fa-f_]+/,
+    )),
+
+    float: $ => token(choice(
+      /-?[0-9_]+\.[0-9_]+/,
+      /[0-9_]+\.[0-9_]*[eE][+-]?[0-9_]+/,
+    )),
+
+    string: $ => choice(
+      $.simple_string,
+      $.interpolated_string,
+    ),
+
+    string_content: $ => /[^"]+/,
+    simple_string: $ => seq(
+      choice('"', '@"'), // @" is verbatim string start
+      optional($.string_content),
+      '"'
+    ),
+    interpolated_string: $ => seq(
+      '$"',
+      repeat(choice($.interpolation, $.string_content)),
+      '"'
+    ),
+    interpolation: $ => seq(
+      "{",
+      $.expression,
+      "}"
+    ),
+
+
+    comment: $ =>
+      token(
+        choice(seq("//", /.*/), seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/")),
+      ),
+
   }
 });
