9fc07ff1585b4af69f39e221adc1dc4498bec9ad
Andrei Voronin <nihlete@gmail.com>
2026-02-22 14:41:25 +0400

    More progress for binary operators
    Also added array and table.

--- grammar.js
+++ grammar.js
@@ -11,6 +11,8 @@ const PREC = {
   UNARY: 1,
   MULTIPLICATIVE: 2,
   ADDITIVE: 3,
+  SHIFT: 4,
+  COMPARISON: 5,
 };
 
 module.exports = grammar({
@@ -43,8 +45,10 @@ module.exports = grammar({
       $.number,
       $.string,
       $.boolean,
+      $.array,
+      $.table,
       $.unary_expression,
-      $.binary_expression
+      $.binary_expression,
     ),
 
     identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
@@ -52,7 +56,7 @@ module.exports = grammar({
     number: $ => choice($.integer, $.float),
 
     integer: $ => token(choice(
-      /-?[1-9][0-9_]*/,
+      /[0-9_]+/,
       /0[xX][0-9A-Fa-f_]+/,
     )),
 
@@ -63,16 +67,26 @@ module.exports = grammar({
 
     string: $ => choice($.simple_string, $.interpolated_string, $.verbatim_string),
 
-    string_content: $ => /[^"]+/,
+    _string_content: $ => /[^"]+/,
     interpolated_string_content: $ => /[^"{}]+/,
-    simple_string: $ => seq('"', optional($.string_content), '"'),
+    simple_string: $ => seq('"', optional($._string_content), '"'),
     interpolated_string: $ => seq('$"', repeat(choice($.interpolation, $.interpolated_string_content)), '"'),
     interpolation: $ => seq("{", $._expression, "}"),
 
-    verbatim_string: $ => seq('@"', repeat(choice($.string_content, $.verbatim_quotes)), '"'),
-    verbatim_quotes: $ => seq('""', optional($.string_content), '""'),
+    verbatim_string: $ => seq('@"', repeat(choice($._string_content, $._verbatim_quotes)), '"'),
+    _verbatim_quotes: $ => seq('""', optional($._string_content), '""'),
 
     boolean: $ => choice("true", "false"),
+
+    array: $ => seq('[', repeat(seq($._expression, optional(','))), ']'),
+
+    table: $ => seq('{', repeat(seq($._table_slot, optional(','))), '}'),
+    _table_slot: $ => choice(
+      $.identifier,
+      seq($.identifier, '=', $._expression),
+      seq('[', $._expression, ']', '=', $._expression),
+      seq($.string , ':', $._expression),
+    ),
 
     unary_expression: $ => choice(
       $._unary_post_expression,
@@ -88,6 +102,16 @@ module.exports = grammar({
       $.multiply,
       $.divide,
       $.mod,
+      $.bit_shift_left,
+      $.bit_shift_right,
+      $.bit_shift_right_unsigned,
+      $.lt,
+      $.gt,
+      $.lteq,
+      $.gteq,
+      $.instanceof,
+      $.in,
+      $.not_in,
     ),
 
     add: $ => prec.left(PREC.ADDITIVE, seq($._expression, '+', $._expression)),
@@ -95,6 +119,16 @@ module.exports = grammar({
     multiply: $ => prec.left(PREC.MULTIPLICATIVE, seq($._expression, '*', $._expression)),
     divide: $ => prec.left(PREC.MULTIPLICATIVE, seq($._expression, '/', $._expression)),
     mod: $ => prec.left(PREC.MULTIPLICATIVE, seq($._expression, '%', $._expression)),
+    bit_shift_left: $ => prec.left(PREC.SHIFT, seq($._expression, '<<', $._expression)),
+    bit_shift_right: $ => prec.left(PREC.SHIFT, seq($._expression, '>>', $._expression)),
+    bit_shift_right_unsigned: $ => prec.left(PREC.SHIFT, seq($._expression, '>>>', $._expression)),
+    lt: $ => prec.left(PREC.COMPARISON, seq($._expression, '<', $._expression)),
+    gt: $ => prec.left(PREC.COMPARISON, seq($._expression, '>', $._expression)),
+    lteq: $ => prec.left(PREC.COMPARISON, seq($._expression, '<=', $._expression)),
+    gteq: $ => prec.left(PREC.COMPARISON, seq($._expression, '>=', $._expression)),
+    instanceof: $ => prec.left(PREC.COMPARISON, seq($._expression, 'instanceof', $._expression)),
+    in: $ => prec.left(PREC.COMPARISON, seq($._expression, 'in', $._expression)),
+    not_in: $ => prec.left(PREC.COMPARISON, seq($._expression, 'not in', $._expression)),
 
     comment: $ =>
       token(
